c64203dfd30e37589a90491d7dc0b63d6c8b9fef
marklearnstocode <markus.danielsen@gmail.com>
2023-09-20 14:04:20 +0200

    Ny opdatering

--- Ny menu/MenuSystem/Program.cs
+++ Ny menu/MenuSystem/Program.cs
@@ -7,31 +7,19 @@
             Menu mainMenu = new Menu();
 
             mainMenu.Title = "Min fantastiske menu";
-            mainMenu.MenuItems = new MenuItem[10];
 
             // First menu item
-            MenuItem mi = new MenuItem();
-            mi.Title = "1. Gør dit";
-            mainMenu.MenuItems[0] = mi;
-            mainMenu.ItemCount++; // Increment with one; same as: ItemCount = ItemCount + 1
+            mainMenu.AddmenuItem("1. Gør dit");
+                 // Increment with one; same as: ItemCount = ItemCount + 1
 
             // Second menu item
-            mi = new MenuItem();
-            mi.Title = "2. Gør dat";
-            mainMenu.MenuItems[1] = mi;
-            mainMenu.ItemCount++;
+            mainMenu.AddmenuItem("2. Gør dat");
 
             // Third menu item
-            mi = new MenuItem();
-            mi.Title = "3. Gør noget";
-            mainMenu.MenuItems[2] = mi;
-            mainMenu.ItemCount++;
+            mainMenu.AddmenuItem("3. Gør noget");
 
             // Last menu item
-            mi = new MenuItem();
-            mi.Title = "4. Få svaret på livet, universet og alting";
-            mainMenu.MenuItems[3] = mi;
-            mainMenu.ItemCount++;
+            mainMenu.AddmenuItem("4. Få svaret på livet, universet og alting");
 
             mainMenu.Show();
 
