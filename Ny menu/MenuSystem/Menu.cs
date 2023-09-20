049e0ab57d99122dbfae91a6c447f8d9224e73ff
marklearnstocode <markus.danielsen@gmail.com>
2023-09-20 13:11:06 +0200

    Test_2

---
+++ Ny menu/MenuSystem/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    public class Menu
+    {
+        public string Title;
+        private MenuItem[] menuItems;
+        private int itemCount = 0;
+        public void Show()
+        {
+            Console.WriteLine(Title + "\n");
+
+            for (int i = 0; i < itemCount; i++)
+                Console.WriteLine(menuItems[i].Title);
+            Console.WriteLine("\n" + "(Tryk menupunkt eller 0 for at afslutte) ");
+        }
+        public void AddmenuTitle(string menuTitle)
+        {
+            MenuItem mi = new MenuItem();
+            mi.Title = menuTitle;
+            menuItems[itemCount] = mi;
+            itemCount++;
+            Console.WriteLine(mi.Title[0]);
+        }
+    }
+}
