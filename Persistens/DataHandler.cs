19abb0281a57d0547f2a7a6744c991491d8492c7
marklearnstocode <markus.danielsen@gmail.com>
2023-09-27 10:09:29 +0200

    Adding test.txt-file to test branch

---
+++ Persistens/DataHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Persistens
+{
+    public class DataHandler
+    {
+        private string dataFileName;
+        public string DataFileName
+        {
+            get { return dataFileName; }
+
+        }
+        public DataHandler(string dataFileName)
+        {
+            this.dataFileName = dataFileName;
+        }
+        public void SavePerson(Person person)
+        {
+        StreamWriter SW = new StreamWriter(dataFileName);
+            SW.WriteLine(person.MakeTitle());
+            SW.Close();
+        }
+        public Person LoadPerson()
+        {
+            StreamReader SR = new StreamReader(dataFileName);
+            string lines = SR.ReadLine();
+            string[] pArray = lines.Split(';');
+
+            Person newPerson = new Person(pArray[0], Convert.ToDateTime(pArray[1]),Convert.ToDouble(pArray[2]), Convert.ToBoolean(pArray[3]), Convert.ToInt32(pArray[4]));
+            SR.Close();
+            return newPerson;
+
+        }
+    }
+}
