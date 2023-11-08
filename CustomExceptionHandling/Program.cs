77cc1a982a2fa16f51c7a974bebf7b8a6663639c
marklearnstocode <markus.danielsen@gmail.com>
2023-11-08 09:11:26 +0100

    opdatering

--- CustomExceptionHandling/Program.cs
+++ CustomExceptionHandling/Program.cs
@@ -7,41 +7,41 @@ namespace CustomExceptionHandling
         static void Main(string[] args)
 
         {
-            try
-            {
-                int posValue = TotallySafe.GetValueAtPosition(-15);
+            //try
+            //{
+                int? posValue = TotallySafe.GetValueAtPosition(-4);
 
                 Console.WriteLine(posValue);
 
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
 
-            }
-            catch (NegativeIndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            try
-            {
-                double posValue = TotallySafe.Divider(0);
-                Console.WriteLine(posValue);
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            //}
+            //catch (NegativeIndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //try
+            //{
+            //    double posValue = TotallySafe.Divider(0);
+            //    Console.WriteLine(posValue);
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
 
-            try
-            {
-                int StringToConvert = TotallySafe.StringToInt("Hej mor");
+            //try
+            //{
+            //    int StringToConvert = TotallySafe.StringToInt("Hej mor");
 
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
             Console.ReadLine();
         }
     }
--- TotallySafeLib/TotallySafe.cs
+++ TotallySafeLib/TotallySafe.cs
@@ -17,14 +17,26 @@ namespace TotallySafeLib
         {
             return int.Parse(stringToConvert);
         }
-        public static int GetValueAtPosition(int positionInArray)
+        public static int? GetValueAtPosition(int positionInArray)
         {
-            if (positionInArray < 0)
-                throw new NegativeIndexOutOfRangeException();
-            else if (positionInArray > 4)
-                throw new IndexOutOfRangeException("Du må ikke indtaste højere end 4");
             int[] intArray = { 1, 2, 3, 4, 5 };
-            return intArray[positionInArray];
+            int? heyfriends = null;
+            try
+            {
+                int heyFriends = intArray[positionInArray];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                if (positionInArray < 0)
+                {
+                    Console.WriteLine("din mor");
+                    throw new NegativeIndexOutOfRangeException();
+
+                }
+                else
+                    throw new IndexOutOfRangeException("Du må ikke indtaste højere end 4");
+            }
+            return heyfriends;
         }
     }
 }
