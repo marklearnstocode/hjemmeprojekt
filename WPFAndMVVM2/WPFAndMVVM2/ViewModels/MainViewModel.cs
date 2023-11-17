c2a9b815cb079713108695528a4a74280f18cd7c
marklearnstocode <markus.danielsen@gmail.com>
2023-11-17 11:32:20 +0100

    opdatering

---
+++ WPFAndMVVM2/WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel
+    {
+        private PersonRepository personRepo = new PersonRepository();
+        public List<PersonViewModel> PersonsVM { get; set; }
+
+        public MainViewModel()
+        {
+            PersonsVM = new List<PersonViewModel>();
+            foreach (Person person in personRepo.GetAll())
+            {
+                PersonViewModel PVM = new PersonViewModel(person);
+                PersonsVM.Add(PVM);
+            }
+        }
+
+        // Implement the rest of this MainViewModel class below to 
+        // establish the foundation for data binding !
+
+    }
+}
---
+++ WPFAndMVVM2/WPFAndMVVM2/ViewModels/PersonViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class PersonViewModel
+    {
+        private Person person;
+		private string firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+		private string phone;
+
+		public string Phone
+		{
+			get { return phone; }
+			set { phone = value; }
+		}
+
+		public PersonViewModel(Person person)
+		{
+			this.person = person;
+			this.firstName = person.FirstName;
+			this.lastName = person.LastName;
+			this.age = person.Age;
+			this.phone = person.Phone;
+		}
+	}
+}
