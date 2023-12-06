d0f7cba2c14e50d4c3b29413b10a937521de5f51
marklearnstocode <markus.danielsen@gmail.com>
2023-12-06 11:16:39 +0100

    Opdatering af INotifyMessageChanged

--- PR33_ObserverPattern3/PR32ObserverPattern2/Academy.cs
+++ PR33_ObserverPattern3/PR32ObserverPattern2/Academy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PR33ObserverPattern3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 namespace PR32ObserverPattern2
 {
     public delegate void Notifyhandler();
-    public class Academy : Organization
+    public class Academy : Organization, INotifyMessageChanged
     {
         #region Field
         public Notifyhandler MessageChanged;
---
+++ PR33_ObserverPattern3/PR32ObserverPattern2/INotifyMessageChanged.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR33ObserverPattern3
+{
+    public interface INotifyMessageChanged
+    {
+        public void MessageChanged()
+        {
+        }
+
+      
+    }
+}
