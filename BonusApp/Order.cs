d8360854c093d603b558651be12cc96eb21b3e83
marklearnstocode <markus.danielsen@gmail.com>
2023-12-08 11:50:35 +0100

    færdiggjort bousapp

---
+++ BonusApp/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Order
+    {
+        List<Product> products = new List<Product>();
+        public BonusProvider Bonus { get; set; }
+        public void AddProduct(Product p)
+        {
+            products.Add(p);
+        }
+        public double GetValueOfProducts()
+        {
+            double totalvalue = 0;
+            foreach (Product p in products)
+            {
+                totalvalue += p.Value;
+            }
+            return totalvalue;
+        }
+        public double GetBonus()
+        {
+            return Bonus(GetValueOfProducts());
+        }
+        public double GetTotalPrice()
+        {
+            return (GetValueOfProducts() - GetBonus());
+        }
+	}
+}
---
+++ BonusApp/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Product
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private double value;
+
+		public double Value
+		{
+			get { return value; }
+			set { this.value = value; }
+		}
+
+
+	}
+}
